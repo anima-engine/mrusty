f1b14b358b0973cc449368c92624dfb1eac20053
Dragos Tiselice <dragostiselice@gmail.com>
2016-04-13 12:04:10 +0300

    Changed from LGPL to MPL.

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
+// mrusty. mruby safe bindings for Rust
+// Copyright (C) 2016  Dragoș Tiselice
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 extern crate gcc;
 extern crate tar;
 extern crate walkdir;
