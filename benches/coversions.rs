10e0b3e016dcb77f6708dcec50195c9fadeefd0b
Dragos Tiselice <dragostiselice@gmail.com>
2016-05-04 20:57:07 +0300

    Added minimal benchmarking.

---
+++ benches/coversions.rs
@@ -0,0 +1,47 @@
+#![feature(test)]
+
+extern crate test;
+
+#[macro_use]
+extern crate mrusty;
+
+use test::Bencher;
+
+use mrusty::*;
+
+#[bench]
+fn convert_fixnum(b: &mut Bencher) {
+    let mruby = Mruby::new();
+
+    b.iter(|| {
+        let one = mruby.fixnum(1);
+
+        one.to_i32().unwrap()
+    });
+}
+
+#[bench]
+fn convert_string(b: &mut Bencher) {
+    let mruby = Mruby::new();
+
+    b.iter(|| {
+        let string = mruby.string("hi");
+
+        string.to_str().unwrap()
+    });
+}
+
+#[bench]
+fn convert_obj(b: &mut Bencher) {
+    struct Cont;
+
+    let mruby = Mruby::new();
+
+    mruby.def_class_for::<Cont>("Container");
+
+    b.iter(|| {
+        let obj = mruby.obj(Cont);
+
+        obj.to_obj::<Cont>().unwrap()
+    });
+}
---
+++ benches/speedup.rs
@@ -0,0 +1,52 @@
+#![feature(test)]
+
+extern crate test;
+
+#[macro_use]
+extern crate mrusty;
+
+use test::Bencher;
+
+use mrusty::*;
+
+#[bench]
+fn fib_rust(b: &mut Bencher) {
+    let mruby = Mruby::new();
+
+    fn fib(n: i32) -> i32 {
+        if n <= 2 {
+            1
+        } else {
+            fib(n - 1) + fib(n - 2)
+        }
+    }
+
+    mruby_class!(mruby, "Fib", {
+        def_self!("fib", |mruby, _slf: Value, n: i32| {
+            mruby.fixnum(fib(n))
+        });
+    });
+
+    b.iter(|| {
+        mruby.run("Fib.fib 20").unwrap()
+    });
+}
+
+#[bench]
+fn fib_ruby(b: &mut Bencher) {
+    let mruby = Mruby::new();
+
+    mruby.run("
+      def fib(n)
+        if n <= 2
+          1
+        else
+          fib(n - 1) + fib(n - 2)
+        end
+      end
+    ").unwrap();
+
+    b.iter(|| {
+        mruby.run("fib 20").unwrap()
+    });
+}
